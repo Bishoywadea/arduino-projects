aa3f763edb60853e67430aac004b87beefaf7f44
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2022-09-14 04:42:35 +0200

    v1.3

--- dataBaseSimu/dataBaseSimu.ino
+++ dataBaseSimu/dataBaseSimu.ino
@@ -4,6 +4,7 @@
 #define led2 5
 #define data 6
 int b1,b2;
+int c1=0,c2=0;
 bool flag1=false,flag2=false,flag=false;
 long long t1=0,t2=0;
 long long inter1,inter2;
@@ -20,11 +21,11 @@ void loop()
 { 
   b1=digitalRead(bt1);
   b2=digitalRead(bt2);
-  if(b1){digitalWrite(led1,HIGH);t1=millis();flag=true;inter1=0;t2=millis();}
+  if(b1){c1++;if(c1%2==0){digitalWrite(led1,LOW);}else{digitalWrite(led1,HIGH);};t1=millis();flag=true;inter1=0;t1=millis();}
   if(flag){inter1=millis()-t1;}
-  if(b2){digitalWrite(led2,HIGH);t2=millis();flag=true;inter1=0;t1=millis();}
+  if(b2){c2++;if(c2%2==0){digitalWrite(led2,LOW);}else{digitalWrite(led2,HIGH);};t2=millis();flag=true;inter1=0;t1=millis();}
   if(flag){inter1=millis()-t1;}
-  if(inter1>3000)
+  if(inter1>10000)
   {
     digitalWrite(data,HIGH);
     inter1=0;
