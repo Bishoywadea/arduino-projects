8d3e88caa72f2057c7585611f600751bf8bf4219
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2022-09-14 04:40:19 +0200

    v1.2

--- dataBaseSimu/dataBaseSimu.ino
+++ dataBaseSimu/dataBaseSimu.ino
@@ -2,6 +2,7 @@
 #define bt2 3
 #define led1 4
 #define led2 5
+#define data 6
 int b1,b2;
 bool flag1=false,flag2=false,flag=false;
 long long t1=0,t2=0;
@@ -12,21 +13,20 @@ void setup(){
     pinMode(bt2,INPUT);
     pinMode(led1,OUTPUT);
     pinMode(led2,OUTPUT);
+    pinMode(data,OUTPUT);
 }
 
 void loop()
 { 
   b1=digitalRead(bt1);
   b2=digitalRead(bt2);
-  if(b1){t1=millis();flag=true;flag1=true;inter1=0;t2=millis();}
+  if(b1){digitalWrite(led1,HIGH);t1=millis();flag=true;inter1=0;t2=millis();}
   if(flag){inter1=millis()-t1;}
-  if(b2){t2=millis();flag=true;flag2=true;inter1=0;t1=millis();}
+  if(b2){digitalWrite(led2,HIGH);t2=millis();flag=true;inter1=0;t1=millis();}
   if(flag){inter1=millis()-t1;}
-  //if(flag2){inter2=millis()-t2;}
   if(inter1>3000)
   {
-    if(flag1){digitalWrite(led1,HIGH);}
-    if(flag2){digitalWrite(led2,HIGH);}
+    digitalWrite(data,HIGH);
     inter1=0;
     flag1=false;
     flag2=false;
