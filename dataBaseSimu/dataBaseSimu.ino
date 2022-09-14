d5df22e402f7e6f45a06df63ab126ed363224467
Bishoywadea <108888519+Bishoywadea@users.noreply.github.com>
2022-09-14 04:39:03 +0200

    v1.1

--- dataBaseSimu/dataBaseSimu.ino
+++ dataBaseSimu/dataBaseSimu.ino
@@ -1,30 +1,36 @@
-#include <Keypad.h>
-
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //three columns
-char keys[ROWS][COLS] = {
-    {'1','2','3','A'},
-    {'4','5','6','B'},
-    {'7','8','9','C'},
-    {'*','0','#','D'}
-};
-
-byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
-char stored[]="AB1234";
-long long t=0,inter=0;
-bool flag=false;
-char key;
-long index=0;
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+#define bt1 2
+#define bt2 3
+#define led1 4
+#define led2 5
+int b1,b2;
+bool flag1=false,flag2=false,flag=false;
+long long t1=0,t2=0;
+long long inter1,inter2;
 void setup(){
     Serial.begin(9600);
+    pinMode(bt1,INPUT);
+    pinMode(bt2,INPUT);
+    pinMode(led1,OUTPUT);
+    pinMode(led2,OUTPUT);
 }
 
 void loop()
-{   Serial.print(index);
-    key=keypad.getKey();
-    if(key){t=millis();flag=true;}
-    if(flag){inter=millis()-t;}
-    if(inter>=3000){Serial.println("pressed");flag=false;inter=0;}
+{ 
+  b1=digitalRead(bt1);
+  b2=digitalRead(bt2);
+  if(b1){t1=millis();flag=true;flag1=true;inter1=0;t2=millis();}
+  if(flag){inter1=millis()-t1;}
+  if(b2){t2=millis();flag=true;flag2=true;inter1=0;t1=millis();}
+  if(flag){inter1=millis()-t1;}
+  //if(flag2){inter2=millis()-t2;}
+  if(inter1>3000)
+  {
+    if(flag1){digitalWrite(led1,HIGH);}
+    if(flag2){digitalWrite(led2,HIGH);}
+    inter1=0;
+    flag1=false;
+    flag2=false;
+    flag=false;
+  }
+  
 }
